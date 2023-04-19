a37d833ac1dc3b5156a2f3d2cf8cf4479e5f9025
Jibola Jinadu <Jibolajinadus8@gmail.com>
2023-04-20 00:58:45 +0100

    firstt commit

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MovieApp from './MovieApp'
+import Star from './Star.png';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <img src={Star} alt="logo" className="logo"/>
+        <MovieApp/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
